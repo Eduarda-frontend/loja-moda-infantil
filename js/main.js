1f02691042bfd27f97944d73c9a44bf17f668448
Maria Eduarda <soaressilvamariaeduarda639@gmail.com>
2024-06-24 22:03:07 -0300

    Projeto responsivo

---
+++ js/main.js
@@ -0,0 +1,39 @@
+$(document).ready(function(){
+
+    $('body').scrollspy({ target: '#cabecalho', offset:10 }); 
+
+    $('#telefone').mask('(00) 00000-0000',{
+        placeholder:'(00) 00000-00000'
+    });
+
+    $('form').validate({
+        rules:{
+            nome:{
+                required:true
+            },
+            email:{
+                required:true,
+                email:true
+            },
+            mensagem:{
+                required:true
+            }
+        },
+        messages:{
+            nome:'Por favor insira o seu nome!'
+        },
+
+        submitHandler: function (form){
+            console.log(form);
+        },
+
+        invalidHandler: function (evento, validador){
+            let camposIncorretos = validador.numberOfInvalids();
+            if (camposIncorretos){
+                alert (`Existem ${camposIncorretos} campos incorretos!`);
+            };
+        }
+    });
+
+
+});
